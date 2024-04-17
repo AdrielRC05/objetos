2e825d5040d523f99f50a24b240f1016b53fc034
adriel <adriel.ruano@iessanmamede.com>
2024-04-17 13:57:57 +0200

    Fin clase

--- src/arrayList_HashMap2/Ejercicio9.java
+++ src/arrayList_HashMap2/Ejercicio9.java
@@ -15,30 +15,29 @@ public class Ejercicio9 {
         ajedrez.put("peon",1);
         ArrayList<Integer> piezasComidas;
         Random aleatorio= new Random();
-        Integer contador, i;
-        do {
-            contador=0;
-            i=0;
-            piezasComidas= new ArrayList<>();
-            piezasComidas.add(aleatorio.nextInt(0,8));
-            piezasComidas.add(aleatorio.nextInt(0,3));
-            piezasComidas.add(aleatorio.nextInt(0,3));
-            piezasComidas.add(aleatorio.nextInt(0,3));
-            piezasComidas.add(aleatorio.nextInt(0,2));
+        int contador, i;
 
-            for (HashMap.Entry e:ajedrez.entrySet()){
-                contador= contador+(Integer) e.getValue()*piezasComidas.get(i);
-                i++;
-            }
-        } while (contador>15);
+        contador=0;
+        i=0;
+        piezasComidas= new ArrayList<>();
+        piezasComidas.add(aleatorio.nextInt(0,9));
+        piezasComidas.add(aleatorio.nextInt(0,3));
+        piezasComidas.add(aleatorio.nextInt(0,3));
+        piezasComidas.add(aleatorio.nextInt(0,3));
+        piezasComidas.add(aleatorio.nextInt(0,2));
+
+        for (HashMap.Entry<String, Integer> e:ajedrez.entrySet()){
+            contador= contador+ e.getValue()*piezasComidas.get(i);
+            i++;
+        }
+
         i=0;
         System.out.println(contador);
         System.out.println("Fichas capturadas por el jugador:");
-        for (HashMap.Entry e:ajedrez.entrySet()){
+        for (HashMap.Entry<String, Integer> e:ajedrez.entrySet()){
             System.out.println(e.getKey()+" ("+piezasComidas.get(i)+")");
             i++;
         }
         System.out.println("Puntos totales: "+contador+" peones.");
-        System.out.println("Hola");
     }
 }
